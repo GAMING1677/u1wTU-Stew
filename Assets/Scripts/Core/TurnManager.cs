d711ea0d4611a4d1daa1a29575c3abcd36ec7020
kokokomomoriri <kichisyumi@yahoo.co.jp>
2025-12-22 22:36:45 +0900

    コアロジック実装

---
+++ Assets/Scripts/Core/TurnManager.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace ApprovalMonster.Core
+{
+    public class TurnManager : MonoBehaviour
+    {
+        public enum TurnPhase
+        {
+            StartStep,
+            PlayerAction,
+            EndStep,
+            Result,
+            GameOver
+        }
+
+        [SerializeField] private TurnPhase currentPhase;
+        public TurnPhase CurrentPhase => currentPhase;
+
+        public UnityEvent OnTurnStart;
+        public UnityEvent OnTurnEnd;
+        public UnityEvent<int> OnTurnChanged;
+
+        private int turnCount;
+
+        public void StartGame()
+        {
+            turnCount = 1;
+            SetPhase(TurnPhase.StartStep);
+        }
+
+        public void StartTurn()
+        {
+            SetPhase(TurnPhase.StartStep);
+        }
+
+        public void EndPlayerAction()
+        {
+            if (currentPhase == TurnPhase.PlayerAction)
+            {
+                SetPhase(TurnPhase.EndStep);
+            }
+        }
+
+        private void SetPhase(TurnPhase nextPhase)
+        {
+            currentPhase = nextPhase;
+            
+            switch (currentPhase)
+            {
+                case TurnPhase.StartStep:
+                    OnTurnStart?.Invoke();
+                    OnTurnChanged?.Invoke(turnCount);
+                    // Proceed to action automatically or after animation
+                    SetPhase(TurnPhase.PlayerAction);
+                    break;
+                case TurnPhase.PlayerAction:
+                    // Enable Input
+                    break;
+                case TurnPhase.EndStep:
+                    OnTurnEnd?.Invoke();
+                    turnCount++;
+                    // Go to next turn start (or Draft phase if implemented later)
+                    StartTurn(); 
+                    break;
+                case TurnPhase.Result:
+                case TurnPhase.GameOver:
+                    break;
+            }
+        }
+    }
+}
